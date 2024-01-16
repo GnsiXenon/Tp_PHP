530f6230624fe5d60e143b00a4c428b3ab4576db
okylian <kylian.oger@ynov.com>
2024-01-16 09:48:11 +0100

    update db

--- RestApi/database/migrations/2023_12_12_070320_create_users_table.php
+++ RestApi/database/migrations/2023_12_12_070320_create_users_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->string('email')->unique(); // Email of the user
             $table->boolean('email_verified')->default(false); // Email verified of the user
             $table->string('password'); // Password of the user
-            $table->rememberToken(); // Remember token for the user
+            $table->rememberToken()->nullable(); // Remember token for the user
             $table->string('api_token', 80)->unique()->nullable()->default(null); // API token for the user
             $table->string('avatar')->nullable(); // Avatar of the user
             $table->timestamps();
